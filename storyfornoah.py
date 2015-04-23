21f78c60ba1cbd1c0de2852ef88f63e2c4d1f8b0
Charlie Johnson <Cjohnson_H@dev.bse.edu>
2015-04-23 15:49:39 -0500

    Update storyfornoah.py

--- storyfornoah.py
+++ storyfornoah.py
@@ -9,7 +9,7 @@ heroesSW = ['Luke Skywalker', 'Yoda', 'Han Solo', 'R2D2', 'C3PO', 'Chewbacca', '
 villainsSW=['Darth Vader','The Emperor', 'Jabba the Hut', ] # I am setting the value for Villains
 heroesTN = ['Leonardo', 'Michelangelo', 'Donatello', 'Rapheal', 'Casey Jones', 'April', 'Master Splinter']
 villainsTN = ['Shredder', 'Rock Steady', 'Bee Bop',]
-
+global chosenlist
 
 def Heropicker(list1,list2): #this is the definition, Heropicker, that is using heroesSW array and heroesTN array
 	herolist = [] #this herolist is empty so that the user can input his/her choices, and it'll show up in the herolist
@@ -20,7 +20,7 @@ def Heropicker(list1,list2): #this is the definition, Heropicker, that is using 
 		
 		uchoice	= raw_input("Make your hero selection here:") #this is where the user will be asked to make his/her selection 
 		
-		herolist.append(uchoice) #here, python is appeneding (adding) the users choice into the herolist 
+		herolist.append(uchoice) #here, python is appeneding the users choice into the herolist 
 		
 	return herolist # after all the choices are made, this will return the herolist 
 		
@@ -32,13 +32,13 @@ print chosenlist #I am assigning the value of Heropicker to the variable chosenl
 
     
 	
-def Villainpicker(list1,list2): #this is the definition,Villainpicker, that is using heroesSW array and heroesTN array
+def Villainpicker(list1,list2): #this is the definition: Villainpicker, that is using heroesSW array and heroesTN array
 	villainlist = []
 	for iter in range(len(list1)):
 		print list1[iter] +" <-> "+ list2[iter]
 		uchoice = raw_input("Make your villain selection here:")
 		villainlist.append(uchoice)
-	return villainlist
+	return  villainlist
 	
 villainlist = Villainpicker(villainsSW, villainsTN)
 print chosenlist 
